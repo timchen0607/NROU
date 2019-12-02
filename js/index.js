846511d60ec8f5a4184b9c26deab118e2dcf4f25
Tim <a0963573232@gmail.com>
2019-12-03 00:10:12 +0800

    create aside 20191203

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".aside_btn").click(function(e) {
+    e.preventDefault();
+    $("#aside").toggleClass("active");
+    $("#navbar").removeClass("show");
+  });
+  $(".nav_btn").click(function(e) {
+    e.preventDefault();
+    $("#aside").removeClass("active");
+  });
+});
